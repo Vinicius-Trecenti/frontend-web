0a932953ad112d74813a4a9abd0f0a374be2b17f
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-05-06 21:49:45 -0300

    Add button createCard

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -28,30 +28,34 @@ const allStocks = [
 ]
 
 
-function addCard(stock) {
+function addCard( { bolsa, codigo, valor, variacao, nAcoes, empresa } ) {
+
+
+    //const { bolsa, codigo, valor, variacao, nAcoes, empresa } = stock
+    
     //document é a estrutura e o query seleciona
     const main = document.querySelector('body > main')
     main.innerHTML = main.innerHTML + `
     <div class="card-tincker">
             <header>
-                <h2><span> ${stock.bolsa}:</span> ${stock.codigo}</h2>
-                <h1>${stock.empresa}</h1>
+                <h2><span> ${bolsa}:</span> ${codigo}</h2>
+                <h1>${empresa}</h1>
             </header>
             <main>
-                <p>R$ ${(+stock.valor / 100).toFixed(2).toString().replace('.', ',')}</p>
+                <p>R$ ${(+valor / 100).toFixed(2).toString().replace('.', ',')}</p>
 
-                <span ${stock.variacao < 0 ? 'style="background: red;"' : ''}> ${stock.variacao < 0 ? '▼' : '▲'} ${stock.variacao}%</span>
+                <span ${variacao < 0 ? 'style="background: red;"' : ''}> ${variacao < 0 ? '▼' : '▲'} ${variacao}%</span>
                 
-                <span>R$ ${realFormat(((+stock.valor / 100) * (stock.variacao / 100)))}</span>
+                <span>R$ ${realFormat(((+valor / 100) * (variacao / 100)))}</span>
 
             </main>
             <footer>
                 <div>
-                    <p>${stock.nAcoes}</p>
+                    <p>${nAcoes}</p>
                     <span>Ações</span>
                 </div>
                 <div>
-                    <p>R$ ${realFormat(stock.nAcoes * (stock.valor / 100))}</p>
+                    <p>R$ ${realFormat(nAcoes * (valor / 100))}</p>
                     <span>Posição</span>
                 </div>
             </footer>
@@ -90,8 +94,11 @@ function loadTable(){
 }
 
 function addRows(stock) {
+
+    //const {bolsa, codigo, valor, variacao, nAcoes, empresa} = stock
+
     const tr = document.querySelector('#acoes')
-    tr.innerHTML = tr.innerHTML + `
+    tr.innerHTML += `
     <tr>
         <td>${stock.bolsa}</td>
         <td>${stock.codigo}</td>
@@ -101,4 +108,42 @@ function addRows(stock) {
         <td>${stock.nAcoes}</td>
         <td>R$ ${realFormat(stock.nAcoes * (stock.valor / 100))}</td>
     </tr>`
+
+    //se colocar table no primeiro tr ele pega a tabela ai só colocar as linhas em uma variavel 
+    //e adicionar nas tabelas para ficar diferente as linhas
+}
+
+const openModal = () => {
+    const modal = document.getElementById('add-card-modal')
+    modal.style.display = 'flex'
+}
+
+const closeModal = (event, id) => {
+    const modal = document.getElementById('add-card-modal')
+    if(event?.target?.id === 'add-card-modal' || id === 'add-card-modal')
+    {
+        modal.style.display = 'none'
+    }
+}
+
+const createCard = (event) => {
+    //para o efeito padrao de redirecionar o formulario
+    event.preventDefault()
+
+    const {bolsa, codigo, valor, variacao, nAcoes, empresa} = event.target.elements
+
+    addCard({
+        bolsa: bolsa.value,
+        codigo: codigo.value,
+        valor: valor.value,
+        variacao: variacao.value,
+        nAcoes: nAcoes.value,
+        empresa: empresa.value
+    })
+
+
+    //limpar o formulario
+    event.target.reset()
+
+    closeModal(null, 'add-card-modal')
 }
