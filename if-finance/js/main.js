225db27375d8fd252af8ef77391a821d7e36df9c
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-05-13 22:18:27 -0300

    add with api

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -1,5 +1,8 @@
 console.log("Arquivo js externo")
 
+// adicione seu token
+const token = 'cp1bhi1r01qu1k1i3j00cp1bhi1r01qu1k1i3j0g'
+
 const allStocks = [
     {
         bolsa: 'NASDAQ',
@@ -28,11 +31,10 @@ const allStocks = [
 ]
 
 
-function addCard( { bolsa, codigo, valor, variacao, nAcoes, empresa } ) {
-
+function addCard({ bolsa, codigo, valor, variacao, nAcoes, empresa }) {
 
     //const { bolsa, codigo, valor, variacao, nAcoes, empresa } = stock
-    
+
     //document é a estrutura e o query seleciona
     const main = document.querySelector('body > main')
     main.innerHTML = main.innerHTML + `
@@ -88,7 +90,7 @@ function loadCards() {
 
 //const showStock2 = stock => addCard(stock)
 
-function loadTable(){
+function loadTable() {
     console.log(allStocks.nAcoes)
     allStocks.map(stock => addRows(stock))
 }
@@ -113,37 +115,83 @@ function addRows(stock) {
     //e adicionar nas tabelas para ficar diferente as linhas
 }
 
-const openModal = () => {
-    const modal = document.getElementById('add-card-modal')
+const openModal = (idModal) => {
+    const modal = document.getElementById(idModal)
     modal.style.display = 'flex'
 }
 
 const closeModal = (event, id) => {
-    const modal = document.getElementById('add-card-modal')
-    if(event?.target?.id === 'add-card-modal' || id === 'add-card-modal')
-    {
-        modal.style.display = 'none'
-    }
+    if(id){
+		const modal = document.getElementById(id)
+		modal.style.display = 'none'
+		return
+	}
+
+	if(event?.target?.className === "modal"){
+		const modal = document.getElementById(event.target.id)
+		modal.style.display = 'none'
+		return
+	}
 }
 
 const createCard = (event) => {
     //para o efeito padrao de redirecionar o formulario
     event.preventDefault()
 
-    const {bolsa, codigo, valor, variacao, nAcoes, empresa} = event.target.elements
+    //const {bolsa, codigo, valor, variacao, nAcoes, empresa} = event.target.elements
 
-    addCard({
-        bolsa: bolsa.value,
-        codigo: codigo.value,
-        valor: valor.value,
-        variacao: variacao.value,
-        nAcoes: nAcoes.value,
-        empresa: empresa.value
-    })
+    //addCard({
+    //    bolsa: bolsa.value,
+    //    codigo: codigo.value,
+    //    valor: valor.value,
+    //    variacao: variacao.value,
+    //    nAcoes: nAcoes.value,
+    //    empresa: empresa.value
+    //})
 
+    const formData = new FormData(event.target)
+    const dados = Object.fromEntries(formData)
+    addCard(dados)
 
     //limpar o formulario
     event.target.reset()
 
     closeModal(null, 'add-card-modal')
+}
+
+//async function testeapi() {}
+const testeapi = async () => {
+    
+    const response = await fetch('https://cat-fact.herokuapp.com/facts')
+    console.log(response)
+    const result = await response.json()
+    console.log(result[0].text)
+
+    result.map(item => console.log(item.text))
+}
+
+
+const createApiCard = async (event) =>{
+	event.preventDefault()
+	const {codigo, nAcoes} = event.target.elements
+
+	const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${codigo.value}&token=${token}`)
+	const result = await response.json()
+
+	const response2 = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${codigo.value}&token=${token}`)
+	const profile = await response2.json()
+
+	const stock = {
+		bolsa: profile.exchange.split(' ')[0],
+		codigo: codigo.value,
+		empresa: profile.name,
+		valor: result.c * 100,
+		variacao: result.d,
+		nAcoes: nAcoes.value
+	}
+
+    console.log(stock)
+	addCard(stock)
+	event.target.reset()
+	closeModal(null, 'add-api-modal')
 }
